8c56a539e2892a75874265b5316f346b521e3a2e
sandy <sandychan19980106@gmail.com>
2025-01-12 16:25:37 +0800

    fix title

---
+++ 第一週/js-type.js
@@ -0,0 +1,12 @@
+function openmenu(){
+    //取得標籤物件
+    let menushow=document.querySelector('#hidden');
+    //操作標籤物件
+    menushow.style.display='block';
+}
+function closemenu(){
+    //取得標籤物件
+    let closemenu=document.querySelector('#hidden');
+    //操作標籤物件
+    closemenu.style.display='none';
+}
